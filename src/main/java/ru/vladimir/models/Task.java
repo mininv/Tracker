0214a285074146e80b866c81eb7611e6be467742
mininva <vovek@bk.ru>
2016-05-25 13:15:13 +0300

    2 поправка

--- src/main/java/ru/vladimir/models/Task.java
+++ src/main/java/ru/vladimir/models/Task.java
@@ -3,9 +3,7 @@ import java.util.*;
 
 public class Task extends Item{
   public Task(String name, String desc, long crea){
-    this.name = name;
-    this.description = desc;
-    this.create = crea;
+    super(name, desc, crea);
   }
   public String calculatePrice(){
     return "100%";
