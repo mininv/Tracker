4afc1dd1aed606a65b8104b84430991fb4418f40
mininva <vovek@bk.ru>
2016-06-17 16:17:02 +0300

    new

---
+++ src/main/java/ru/vladimir/start/ValidateInput.java
@@ -0,0 +1,21 @@
+package ru.vladimir.start;
+
+public class ValidateInput extends ConsoleInput{
+  public int ask(String question, int[] range){
+    boolean invalid = true;
+    int value = -1; 
+    do{
+      try{
+        value = super.ask(question, range);//родительский конструтор метода ask
+        invalid = false;
+      }catch(MenuOutException moe){
+         moe.printStackTrace();
+         System.out.println("Please select key from menu. ");
+       }
+      catch(NumberFormatException nfe){
+         System.out.println("Please enter validate data again. ");
+       }
+    }while(invalid);
+    return value;  
+  }
+}
